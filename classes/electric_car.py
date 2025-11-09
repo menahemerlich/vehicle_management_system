8dbdb62e9d77c7c1716d052a6cc4e190e244bcc1
menahem_erlich <erlichmenahem@gmail.com>
2025-11-09 12:49:16 +0200

    finish

--- classes/electric_car.py
+++ classes/electric_car.py
@@ -3,7 +3,9 @@ from classes.electric_mixin import ElectricMixin
 from classes.luxury_mixin import LuxuryMixin
 
 class ElectricCar(Car, ElectricMixin, LuxuryMixin):
-    pass
+    def calculate_annual_tax(self):
+        self.tax = 250
+        return self.tax
 
 
 
--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+from classes.electric_car import ElectricCar
+from classes.car import Car
+from classes.truck import Truck
+from classes.engine import Engine
 
+car1 = Car(1504726, 1998, Engine(170, "MINI"))
+e_car1 = ElectricCar(20284703, 2015, Engine(150, "BMW"))
+truck1 = Truck(40156201, 2005, 350)
+vehicles = [car1, e_car1, truck1]
+for vehicle in vehicles:
+    print(vehicle.calculate_annual_tax())
+
+e_car1.set_year(2021)
+print(e_car1.get_year())
+e_car1.charge()
+l = ["elctric", "blake"]
+print(e_car1.get_luxury_features(l))
 
 
 
